ee16bce9aa4e44ae002d648d375ad69fcb28e08a
Hong1008 <46987508+Hong1008@users.noreply.github.com>
2020-03-15 15:50:23 +0900

    설정

---
+++ src/main/java/remasterWithJpa/ohRecipe/OhRecipeApplication.java
@@ -0,0 +1,13 @@
+package remasterWithJpa.ohRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhRecipeApplication.class, args);
+	}
+
+}
